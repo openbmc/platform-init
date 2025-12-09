da4240cfbc3a86e9c33f7aaed3a66f723adb613d
Dawid Glazik <dawid.glazik@intel.com>
2025-12-09 15:55:50 +0100

    Add Intel platforms
    Adds Intel acrp and Intel jcrp targets.
    
    Tested:
    Ran on acrp as well as jcrp. Both platforms booted correctly.
    
    Change-Id: I602f903d19a214dee2d340e8e91b8c4da1a7db2e
    Signed-off-by: Dawid Glazik <dawid.glazik@intel.com>

---
+++ intel/jcrp.cpp
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: Apache-2.0
+// SPDX-FileCopyrightText: Copyright OpenBMC Authors
+
+#include "gpio.hpp"
+#include <systemd/sd-daemon.h>
+#include <iostream>
+
+namespace intel
+{
+
+int init_jcrp()
+{
+    std::cerr << "Initializing Intel JCRP platform...\n";
+    
+    gpio::set_raw(2, 19, 1);
+    gpio::set_raw(2, 49, 1);
+    gpio::set_raw(2, 57, 1);
+    gpio::set_raw(2, 23, 1);
+
+    std::cerr << "Intel JCRP platform initialization complete\n";
+    sd_notify(0, "READY=1");
+    
+    return EXIT_SUCCESS;
+}
+
+} // namespace intel
