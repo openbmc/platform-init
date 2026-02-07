427e675e83464a9f78ac3f48178a44bc51828a09
Oliver Brewka <oliver.brewka@9elements.com>
2026-02-07 01:13:56 +0100

    WIP: Add initial meta catalina
    Add the minimum requirement for meta catalina platform init
    
    Change-Id: I7fdfb31f4401a6af064ac29f4f33b667d360097b
    Signed-off-by: Oliver Brewka <oliver.brewka@9elements.com>

---
+++ meta/meta.hpp
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: Apache-2.0
+// SPDX-FileCopyrightText: Copyright OpenBMC Authors
+
+#pragma once
+
+namespace meta
+{
+
+// list your platform initialization callbacks here
+int init_catalina_base();
+
+} // namespace meta
