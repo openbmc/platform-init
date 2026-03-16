0af6889146579ee8daecadcdfdfe8ff5fea372e6
Oliver Brewka <oliver.brewka@9elements.com>
2026-03-16 06:22:02 +0100

    cmos-reset: Add cmos reset service
    In order to support a cmos reset / clear, add a
    dbus service that exposes the Reset method from
    xyz.openbmc_project.Common.FactoryReset.
    
    Each platform should define its specific cmos reset function which gets
    passed to the service.
    
    Follow-up patches complete the following flow of actions:
    
    1. Check host state
    2. power off, if running
    3. set ActivationBlocksTransition interface
    4. run platform cmos reset function
    5. remove ActivationBlocksTransition interface
    6. restore initial host power state
    7. send success event log
    
    If any step fails, send a failure event log and return early.
    
    The service has been designed with Redfish in mind.
    A user should be able to trigger the reset through Bios.ResetBios
    action, for a) a lack of a better action and b) a cmos reset typically
    is performed to reset the bios.
    
    Currently on the bmcweb side this action calls into
    `org.openpower.Software.Host.Updater /xyz/openbmc_project/software
    xyz.openbmc_project.Common.FactoryReset Reset`. As part of this series,
    the busName shall be changed to
    xyz.openbmc_project.Software.Host.Updater0 [2] in an attempt to unify.
    
    Upgrading the code to be compatible with multi-host platforms in the
    future might require to change the instance path to include the specific
    host segment.
    
    The service builds independently and is opt-in through
    package config in yocto [3].
    
    Tested: Code compiles.
    Build an image that configures to use the service. Run the image and
    check if the service is started correctly. Trigger the the Reset method
    both manually and through Redfish and check journal output for the correct
    debug prints.
    
    [1] https://gerrit.openbmc.org/c/openbmc/bmcweb/+/88227
    [2] https://gerrit.openbmc.org/c/openbmc/openpower-pnor-code-mgmt/+/88228
    [3] https://gerrit.openbmc.org/c/openbmc/openbmc/+/88527
    
    Change-Id: Ia385ab1ccf48b441ffcd449385350c0dec21aae5
    Signed-off-by: Oliver Brewka <oliver.brewka@9elements.com>

--- meta/meta.hpp
+++ meta/meta.hpp
@@ -3,10 +3,13 @@
 
 #pragma once
 
+#include <sdbusplus/async.hpp>
+
 namespace meta
 {
 
 // list your platform initialization callbacks here
 int init_catalina_base();
+sdbusplus::async::task<bool> catalina_cmos_reset(sdbusplus::async::context&);
 
 } // namespace meta
