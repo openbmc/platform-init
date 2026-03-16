d8b1b699c5315d26dff31c5bf6f113faed818581
Oliver Brewka <oliver.brewka@9elements.com>
2026-03-16 06:22:02 +0100

    cmos-reset: Add cmos reset service
    In order to support a cmos reset / clear, add a
    dbus service that exposes the Reset method from
    xyz.openbmc_project.Common.FactoryReset.
    
    Each platform should define its specific cmos reset function which gets
    passed to the service.
    
    Follow-up patches complete the following flow of actions:
    
    1. Check host state
    2. power off, if running
    3. set ActivationBlocksTransition interface
    4. run platform cmos reset function
    5. remove ActivationBlocksTransition interface
    6. restore initial host power state
    7. send success event log
    
    If any step fails, send a failure event log and return early.
    
    The service has been designed with Redfish in mind.
    A user should be able to trigger the reset through Bios.ResetBios
    action, for a) a lack of a better action and b) a cmos reset typically
    is performed to reset the bios.
    
    Currently on the bmcweb side this action calls into
    `org.openpower.Software.Host.Updater /xyz/openbmc_project/software
    xyz.openbmc_project.Common.FactoryReset Reset`. As part of this series,
    the busName shall be changed to
    xyz.openbmc_project.Software.Host.Updater0 [2] in an attempt to unify.
    
    Upgrading the code to be compatible with multi-host platforms in the
    future might require to change the instance path to include the specific
    host segment.
    
    The service builds independently and is opt-in through
    package config in yocto [3].
    
    Tested: Code compiles.
    Build an image that configures to use the service. Run the image and
    check if the service is started correctly. Trigger the Reset method both
    manually and through Redfish and check journal output for the correct
    debug prints.
    
    [1] https://gerrit.openbmc.org/c/openbmc/bmcweb/+/88227
    [2] https://gerrit.openbmc.org/c/openbmc/openpower-pnor-code-mgmt/+/88228
    [3] https://gerrit.openbmc.org/c/openbmc/openbmc/+/88527
    
    Change-Id: Ia385ab1ccf48b441ffcd449385350c0dec21aae5
    Signed-off-by: Oliver Brewka <oliver.brewka@9elements.com>

---
+++ cmos-reset/cmos_reset.cpp
@@ -0,0 +1,62 @@
+// SPDX-License-Identifier: Apache-2.0
+// SPDX-FileCopyrightText: Copyright OpenBMC Authors
+
+#include "cmos_reset.hpp"
+
+#include <phosphor-logging/lg2.hpp>
+
+PHOSPHOR_LOG2_USING;
+
+namespace cmos
+{
+
+sdbusplus::async::task<> CmosReset::startReset()
+{
+    info("CMOS reset requested");
+
+    bool success = co_await doReset();
+
+    if (!success)
+    {
+        error("CMOS reset failed");
+        co_return;
+    }
+
+    info("CMOS reset successful");
+}
+
+sdbusplus::async::task<bool> CmosReset::doReset()
+{
+    if (!resetMethod)
+    {
+        error("No reset function provided for this platform");
+        co_return false;
+    }
+
+    bool success = co_await resetMethod(ctx);
+    if (!success)
+    {
+        error("CMOS reset failed");
+        co_return false;
+    }
+
+    co_return true;
+}
+
+int init_service(CmosResetMethod resetMethod)
+{
+    sdbusplus::async::context ctx;
+    sdbusplus::server::manager_t manager(ctx, softwarePath);
+
+    CmosReset service(ctx, softwarePath, resetMethod);
+
+    ctx.spawn([](sdbusplus::async::context& ctx) -> sdbusplus::async::task<> {
+        ctx.request_name(busName);
+        co_return;
+    }(ctx));
+
+    ctx.run();
+
+    return EXIT_SUCCESS;
+}
+} // namespace cmos
---
+++ cmos-reset/cmos_reset_main.cpp
@@ -0,0 +1,47 @@
+// SPDX-License-Identifier: Apache-2.0
+// SPDX-FileCopyrightText: Copyright OpenBMC Authors
+
+#include "cmos_reset.hpp"
+#include "meta/meta.hpp"
+
+#include <CLI/CLI.hpp>
+
+#include <array>
+#include <cstdlib>
+#include <string_view>
+
+constexpr std::array<
+    std::pair<std::string_view,
+              sdbusplus::async::task<bool> (*)(sdbusplus::async::context&)>,
+    1>
+    reset_functions{{
+        {"meta-catalina", meta::catalina_cmos_reset},
+    }};
+
+int main(int argc, char** argv)
+{
+    CLI::App app("CMOS Reset Service CLI");
+
+    std::string platform_name;
+
+    app.add_option("platform_name", platform_name, "Name of the platform")
+        ->required(true);
+
+    CLI11_PARSE(app, argc, argv);
+
+    const auto* it = std::ranges::find_if(
+        reset_functions,
+        [&platform_name](
+            const std::pair<std::string_view, sdbusplus::async::task<bool> (*)(
+                                                  sdbusplus::async::context&)>
+                val) { return val.first == platform_name; });
+    if (it == reset_functions.end())
+    {
+        std::cerr << app.help() << "\n";
+        return EXIT_FAILURE;
+    }
+
+    cmos::init_service(it->second);
+
+    return EXIT_SUCCESS;
+}
