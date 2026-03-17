a37862e845c5be155acc494ad2e21b601a0b1e77
Oliver Brewka <oliver.brewka@9elements.com>
2026-03-17 16:43:46 +0100

    WIP: Add power control capabilities
    tbd
    
    Tested: tbd
    
    Change-Id: Ifbcd00d0443ba5292acf9e9a3380deeb16098f9d
    Signed-off-by: Oliver Brewka <oliver.brewka@9elements.com>

--- host_bios_reset.hpp
+++ host_bios_reset.hpp
@@ -3,6 +3,8 @@
 
 #include <sdbusplus/bus.hpp>
 #include <xyz/openbmc_project/Common/FactoryReset/server.hpp>
+#include <xyz/openbmc_project/ObjectMapper/client.hpp>
+#include <xyz/openbmc_project/State/Host/client.hpp>
 
 namespace host
 {
@@ -14,6 +16,14 @@ namespace reset
 constexpr const char* softwarePath = "/xyz/openbmc_project/software";
 constexpr const char* busName = "xyz.openbmc_project.Host.Updater0";
 
+constexpr const char* hostPath = "/xyz/openbmc_project/state/host0";
+constexpr const char* hostBusName = "xyz.openbmc_project.State.Host0";
+
+constexpr const char* propertiesInterface = "org.freedesktop.DBus.Properties";
+
+using ObjectMapper = sdbusplus::client::xyz::openbmc_project::ObjectMapper<>;
+using HostState = sdbusplus::client::xyz::openbmc_project::state::Host<>;
+
 using BiosResetInherit = sdbusplus::server::object_t<
     sdbusplus::server::xyz::openbmc_project::common::FactoryReset>;
 
@@ -24,13 +34,18 @@ class BiosReset : public BiosResetInherit
   public:
     BiosReset(sdbusplus::bus_t& bus, const char* objPath,
               BiosResetMethod resetMethod = nullptr) :
-        BiosResetInherit(bus, objPath), resetMethod(resetMethod)
+        BiosResetInherit(bus, objPath), bus(bus), resetMethod(resetMethod)
     {}
 
     void reset();
 
+  protected:
+    sdbusplus::bus_t& bus;
+
   private:
     BiosResetMethod resetMethod;
+    bool isHostRunning();
+    void requestHostTransition(HostState::Transition transition);
 };
 
 void init_service(BiosResetMethod resetMethod);
