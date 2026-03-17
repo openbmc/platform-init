5fe1dc8a4001b6e05eae21c42669041562c01bd6
Oliver Brewka <oliver.brewka@9elements.com>
2026-03-17 16:43:46 +0100

    cmos-reset: Add power control capabilities
    Ideally the host is powered off before any reset.
    Add functionality, to check if the host is running and to do state
    transitions accordingly.
    
    A sane default behavior is to restore the previously captured host state
    as is done in phosphor-bmc-code-mgmt repository.
    
    In addition to have more control over the transition timeouts, the
    'host-state-transition-timeout' option has been added with a default
    value of 60 seconds.
    
    The majority of the core power logic comes from phosphor-bmc-code-mgmt
    with slight adjustments made. The dbus match got removed, as well as
    some checks of CurrentHostState, which are now done inside the doReset()
    function.
    
    Tested:
    The logic has been tested on a tyan s8047 board.
    Triggered a cmos reset through redfish in both, the 'Off' and the
    'Running' state of the host.
    Validated that the service printed the correct logs as well as checked
    x86-power-control that it registered the correct state transitions.
    Made sure, that when the host initially was 'Running', that state got
    restored  after the reset.
    
    Output for a cmos reset, when host was initially running:
    
    ```
    May 10 13:59:32 s8047 cmos-reset[318]: CMOS reset requested
    May 10 13:59:32 s8047 cmos-reset[318]: Host is running, transitioning to off
    May 10 13:59:33 s8047 cmos-reset[318]: Resetting cmos
    May 10 13:59:38 s8047 cmos-reset[318]: CMOS reset done
    May 10 13:59:39 s8047 cmos-reset[318]: CMOS reset successful
    May 10 13:59:39 s8047 cmos-reset[318]: Restoring previous host state
    May 10 13:59:39 s8047 cmos-reset[318]: Host state restored
    May 10 13:59:39 s8047 cmos-reset[318]: OPENBMC_MESSAGE_ID={"xyz.openbmc_project.Common.FactoryReset.ResetSuccess":{"RESET_CAUSE", "Manual CMOS reset", "SOURCE":"/xyz/openbmc_project/software","_SOURCE":{"COLUMN":77,"FILE":"../../../../../../../../../../openbmc/platform-init/cmos-reset/cmos_reset.cpp","FUNCTION":"sdbusplus::async::task<void> cmos::CmosReset::startReset()","LINE":36,"PID":318}}}
    
    ```
    
    Change-Id: Ifbcd00d0443ba5292acf9e9a3380deeb16098f9d
    Signed-off-by: Oliver Brewka <oliver.brewka@9elements.com>

---
+++ cmos-reset/host_power.cpp
@@ -0,0 +1,50 @@
+#include "host_power.hpp"
+
+#include <phosphor-logging/lg2.hpp>
+
+PHOSPHOR_LOG2_USING;
+
+namespace host_power
+{
+
+sdbusplus::async::task<bool> HostPower::setState(sdbusplus::async::context& ctx,
+                                                 HostState state)
+{
+    auto client = sdbusplus::client::xyz::openbmc_project::state::Host(ctx)
+                      .service(hostStateService)
+                      .path(host0ObjectPath);
+
+    co_await client.requested_host_transition(
+        (state == stateOn) ? transitionOn : transitionOff);
+
+    debug("Requested host transition to {STATE}", "STATE", state);
+
+    for (size_t i = 0; i < HOST_STATE_TRANSITION_TIMEOUT; i++)
+    {
+        co_await sdbusplus::async::sleep_for(ctx, std::chrono::seconds(1));
+
+        if ((co_await client.current_host_state()) == state)
+        {
+            debug("Successfully achieved state {STATE}", "STATE", state);
+            co_return true;
+        }
+    }
+
+    error("Failed to achieve state {STATE} before the timeout of {TIMEOUT}s",
+          "STATE", state, "TIMEOUT", HOST_STATE_TRANSITION_TIMEOUT);
+
+    co_return false;
+}
+
+sdbusplus::async::task<HostState> HostPower::getState(
+    sdbusplus::async::context& ctx)
+{
+    auto client = sdbusplus::client::xyz::openbmc_project::state::Host(ctx)
+                      .service(hostStateService)
+                      .path(host0ObjectPath);
+
+    auto res = co_await client.current_host_state();
+
+    co_return res;
+}
+} // namespace host_power
