2729eab798ba16cfcd154ba7788647800a290d59
Oliver Brewka <oliver.brewka@9elements.com>
2026-03-17 16:59:26 +0100

    WIP: Catalina cmos clear
    tbd
    
    Tested: tbd
    
    Change-Id: I4fb5e3b84680fe2d3c57b7077a2b27f2433ccfaa
    Signed-off-by: Oliver Brewka <oliver.brewka@9elements.com>

--- meta/catalina.cpp
+++ meta/catalina.cpp
@@ -2,6 +2,7 @@
 // SPDX-FileCopyrightText: Copyright OpenBMC Authors
 
 #include "cmos_reset.hpp"
+#include "gpio.hpp"
 #include "meta.hpp"
 
 #include <iostream>
@@ -12,6 +13,14 @@ namespace meta
 void catalina_cmos_reset()
 {
     std::cerr << "CMOS reset triggered\n";
+
+    const char* rtcClr = "RTC_CLR_L";
+
+    gpio::set(rtcClr, 0);
+    std::this_thread::sleep_for(std::chrono::seconds(5));
+    gpio::set(rtcClr, 1);
+
+    return;
 }
 
 int init_catalina_base()
