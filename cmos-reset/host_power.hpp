bfa72ee5a99d35ada574f54d9bc0c8789221121d
Oliver Brewka <oliver.brewka@9elements.com>
2026-03-17 16:43:46 +0100

    cmos-reset: Add power control capabilities
    Ideally the host is powered off before any reset.
    Add functionality, to check if the host is running and to do state
    transitions accordingly.
    
    A sane default behavior is to restore the previously captured host state
    as is done in phosphor-bmc-code-mgmt repository.
    
    In addition to have more control over the transition timeouts, the
    'host-state-transition-timeout' option has been added with a default
    value of 60.
    
    The majority of the core power logic comes from phosphor-bmc-code-mgmt
    with slight adjustments made. The dbus match got removed, as well as
    some checks of CurrentHostState, which are now done inside the doReset()
    function.
    
    Tested:
    Build the image and run it in qemu. Trigger a reset while
    CurrentHostState is set to 'Off' and observe the reset going through.
    
    tbd Testing on hw for host 'Running' case.
    
    Change-Id: Ifbcd00d0443ba5292acf9e9a3380deeb16098f9d
    Signed-off-by: Oliver Brewka <oliver.brewka@9elements.com>

---
+++ cmos-reset/host_power.hpp
@@ -0,0 +1,48 @@
+#pragma once
+
+#include "config.h"
+
+#include <phosphor-logging/lg2.hpp>
+#include <sdbusplus/async.hpp>
+#include <sdbusplus/async/context.hpp>
+#include <xyz/openbmc_project/State/Host/client.hpp>
+
+PHOSPHOR_LOG2_USING;
+
+namespace host_power
+{
+
+const auto stateOn =
+    sdbusplus::client::xyz::openbmc_project::state::Host<>::HostState::Running;
+const auto stateOff =
+    sdbusplus::client::xyz::openbmc_project::state::Host<>::HostState::Off;
+const auto transitionOn =
+    sdbusplus::client::xyz::openbmc_project::state::Host<>::Transition::On;
+const auto transitionOff =
+    sdbusplus::client::xyz::openbmc_project::state::Host<>::Transition::Off;
+
+using HostState =
+    sdbusplus::client::xyz::openbmc_project::state::Host<>::HostState;
+
+using StateIntf =
+    sdbusplus::client::xyz::openbmc_project::state::Host<void, void>;
+
+const auto host0ObjectPath = sdbusplus::client::xyz::openbmc_project::state::
+                                 Host<>::namespace_path::value +
+                             std::string("/host0");
+
+constexpr const char* hostStateService = "xyz.openbmc_project.State.Host0";
+
+class HostPower
+{
+  public:
+    // @param state   desired powerstate
+    // @returns       true on success
+    static sdbusplus::async::task<bool> setState(sdbusplus::async::context& ctx,
+                                                 HostState state);
+
+    // @returns       host powerstate
+    static sdbusplus::async::task<HostState> getState(
+        sdbusplus::async::context& ctx);
+};
+}; // namespace host_power
