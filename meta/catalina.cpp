39feb90bffb7c91f5673b6e6c01d1fdfa812b8b6
Oliver Brewka <oliver.brewka@9elements.com>
2026-03-17 16:59:26 +0100

    cmos-reset: Add Catalina cmos reset function
    To serve as an easy example for other platforms to adopt, add a concrete
    implementation of a cmos reset function for the catalina platform.
    
    Tested: Build an image and run it in qemu.
    Verify the function gets called and is executed correctly.
    
    ```
    $ journalctl -u xyz.openbmc_project.Software.Host.Updater0 -f
    
    Apr 09 07:53:53 catalina cmos-reset[388]: CMOS reset requested
    Apr 09 07:53:53 catalina cmos-reset[388]: RTC_CLR_L Request to set to 0
    Apr 09 07:53:53 catalina cmos-reset[388]: RTC_CLR_L Set to 0
    Apr 09 07:54:25 catalina cmos-reset[388]: RTC_CLR_L Request to set to 1
    Apr 09 07:54:25 catalina cmos-reset[388]: RTC_CLR_L Settingto 1
    Apr 09 07:54:25 catalina cmos-reset[388]: CMOS reset successful
    Apr 09 07:54:25 catalina cmos-reset[388]: OPENBMC_MESSAGE_ID={"xyz.openbmc_project.Common.FactoryReset.ResetSuccess":{"RESET_CAUSE", "Manual CMOS reset", "SOURCE":"/xyz/openbmc_project/software","_SOURCE":{"COLUMN":77,"FILE":"../../../../../../../../../../openbmc/platform-init/cmos-reset/cmos_reset.cpp","FUNCTION":"sdbusplus::async::task<void> cmos::CmosReset::startReset()","LINE":33,"PID":388}}}
    
    ```
    Change-Id: I4fb5e3b84680fe2d3c57b7077a2b27f2433ccfaa
    Signed-off-by: Oliver Brewka <oliver.brewka@9elements.com>

--- meta/catalina.cpp
+++ meta/catalina.cpp
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: Apache-2.0
 // SPDX-FileCopyrightText: Copyright OpenBMC Authors
 
+#include "gpio.hpp"
 #include "meta.hpp"
 
 #include <chrono>
@@ -11,8 +12,21 @@ namespace meta
 
 sdbusplus::async::task<bool> catalina_cmos_reset(sdbusplus::async::context& ctx)
 {
-    std::cerr << "CMOS reset triggered\n";
-    sdbusplus::async::sleep_for(ctx, std::chrono::seconds(5));
+    int gpiochipint =
+        gpio::find_chip_idx_from_dir("/sys/bus/i2c/devices/6-0021");
+
+    if (gpiochipint < 0)
+    {
+        std::cerr << "Error: couldn't find gpiochip\n";
+        co_return false;
+    }
+
+    const char* rtcClr = "RTC_CLR_L";
+    const size_t TIMEOUT = 5;
+
+    gpio::set(rtcClr, 0);
+    co_await sdbusplus::async::sleep_for(ctx, std::chrono::seconds(TIMEOUT));
+    gpio::set(rtcClr, 1);
     co_return true;
 }
 
