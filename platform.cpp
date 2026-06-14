a65269220c4b51b2a26f6a9378c73e5971f93d34
Oliver Brewka <oliver.brewka@9elements.com>
2026-06-14 15:14:11 +0200

    Use CLI11 callback function for init sub
    Follow-up patch adds a new sub command. Using CLI11 callback function to
    have uniform code for sub command handling.
    
    Tested: Build a meta-catalina image.
    Upon boot, check the output of `systemctl status platform_init`.
    Confirmed the dummy init function for meta-catalina executed.
    
    Change-Id: I71261b28ee60279c589b50a7ae5943cf3b1b9960
    Signed-off-by: Oliver Brewka <oliver.brewka@9elements.com>

--- platform.cpp
+++ platform.cpp
@@ -28,6 +28,29 @@ constexpr auto init_functions =
          {"nvidia-nvl32", nvidia::init_nvl32},
          {"nvidia-vr-nvl", nvidia::init_vr_nvl}});
 
+void init_sub_callback(std::string& platform_name, bool& success)
+{
+    const auto* it = std::ranges::find_if(
+        init_functions,
+        [&platform_name](const std::pair<std::string_view, int (*)()> val) {
+            return val.first == platform_name;
+        });
+    if (it == init_functions.end())
+    {
+        std::cerr << "no init function";
+        return;
+    }
+
+    int rc = it->second();
+    if (rc != EXIT_SUCCESS)
+    {
+        std::cerr << "init function failed\n";
+        return;
+    }
+
+    success = true;
+}
+
 int main(int argc, char** argv)
 {
     CLI::App app("Platform init CLI");
@@ -37,24 +60,23 @@ int main(int argc, char** argv)
     CLI::App* init_sub =
         app.add_subcommand("init", "Initialize the platform and daemonize");
     std::string platform_name;
+    bool success = false;
     init_sub
         ->add_option("platform_name", platform_name,
                      "Name of the platform to init")
         ->required();
-    app.require_subcommand();
 
+    init_sub->callback([&platform_name, &success]() {
+        init_sub_callback(platform_name, success);
+    });
+
+    app.require_subcommand();
     CLI11_PARSE(app, argc, argv)
 
-    const auto* it = std::ranges::find_if(
-        init_functions,
-        [&platform_name](const std::pair<std::string_view, int (*)()> val) {
-            return val.first == platform_name;
-        });
-    if (it == init_functions.end())
+    if (!success)
     {
-        std::cerr << init_sub->help() << "\n";
         return EXIT_FAILURE;
     }
 
-    return it->second();
+    return EXIT_SUCCESS;
 }
